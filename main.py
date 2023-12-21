f0171aaa9004a31dcfc514ccae8164b44f958c2a
Ashwin Jain <1ashwinjain@gmail.com>
2023-12-21 12:27:22 -0600

    cloud function

--- main.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import firestore
-
-# The `project` parameter is optional and represents which project the client
-# will act on behalf of. If not supplied, the client falls back to the default
-# project inferred from the environment.
-db = firestore.Client(project="my-project-id")
